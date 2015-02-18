493c2754d7d9d4b22907179922cad4db1a1dea41
vagrant <vagrant@vagrant.vm>
2015-02-18 21:05:35 +0000

    Remove edit and destroy for the school index view

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,4 +1,5 @@
 class SchoolsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
   before_action :set_school, only: [:show, :edit, :update, :destroy]
 
   # GET /schools
